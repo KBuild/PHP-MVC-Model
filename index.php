784d6d1bf4d3aa1a2cdaf7e6f62ef401149ac0ea
KBuild <qwer7995@gmail.com>
2012-07-16 16:29:38 +0900

    version 0.1

--- index.php
+++ index.php
@@ -6,5 +6,6 @@ $s = preg_split('/'.basename(__FILE__).'/', $_SERVER['SCRIPT_NAME']);
 define(PATH, dirname(__FILE__));
 define(EXT, '.'.pathinfo(__FILE__, PATHINFO_EXTENSION));
 define(APP_NAME, $s[0]);
+define(VERSION, 0.1);
 require_once('lib/route.php');
 ?>
