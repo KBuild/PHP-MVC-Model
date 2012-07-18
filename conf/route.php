a481c3c70e9b1abc0656c577a5c13f2c1d54c89e
KBuild <qwer7995@gmail.com>
2012-07-18 11:11:39 +0900

    configure update

--- conf/route.php
+++ conf/route.php
@@ -9,11 +9,16 @@
 	 * $route['controller']['N'] : if request got likely www.example.com/index.php/post/1, take a action
 	 *  -> ex) $route['post']['N'] = 'post/show';
 	 *
+	 * $route['controller']['POST'] : if request got likely www.example.com/index.php/post by POST method, take a action
+	 *  -> ex) $route['post']['POST'] = 'post/new';
+	 *
 	 * $route['controller']['action'] : if request got likely www.example.com/index.php/controller/action, take a action
-	 *  -> ex) $route['post']['new'] = 'post/write'
+	 *  -> ex) $route['post']['wow'] = 'post/nowow'
 	 */
 
 	$route['default'] = 'post/index';
 	$route['post']['test2'] = 'post/test';
-	$route['post']['N'] = 'post/show';
+	$route['post']['NUM'] = 'post/show';
+	$route['post']['POST'] = 'post/save';
+	$route['news'] = 'post/test';
 ?>
